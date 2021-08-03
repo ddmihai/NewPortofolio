a55f1f1a51341dcb9a74ab96001d37c0fb62e7c0
daniel mihai <sasdaniel9@gmail.com>
2021-08-03 15:12:13 +0100

    work on progress

--- javascript/index.js
+++ javascript/index.js
@@ -6,6 +6,11 @@ const restaurant    = document.querySelector('.ohmyfood');
 const reservia      = document.querySelector('.reservia-btn');
 const fitnessPal    = document.querySelector('.fitnessPal');
 
+const footerEmail   = document.querySelector('.footerEmail');
+const footerContact = document.querySelector('.footerContact');
+const footerCall    = document.querySelector('.footerCall');
+const footerGit     = document.querySelector('.footerGitNav');
+
 let navtoGit = () => {
     location.href = 'https://github.com/ddmihai';
 }
@@ -16,6 +21,10 @@ let navtoGit = () => {
 gitNav.addEventListener('click',            navtoGit);
 gitNavigation.addEventListener('click',     navtoGit);
 email.addEventListener('click',             () => topnavName.textContent = 'sasdaniel9@gmail.com');
+footerEmail.addEventListener('click',       () => footerContact.textContent = 'Get in touch - sasdaniel9@gmail.com');
+footerCall.addEventListener('click',        () => footerContact.textContent = 'Get in touch - 07887637239');
+
 restaurant.addEventListener('click',        () => location.href = 'https://ddmihai.github.io/restaurant/');
 reservia.addEventListener('click',          () => location.href = 'https://ddmihai.github.io/reservia.io/');
 fitnessPal.addEventListener('click',        () => location.href = 'https://flamboyant-stonebraker-c1c438.netlify.app/#/');
+footerGit.addEventListener('click',         () => location.href = 'https://github.com/ddmihai');
