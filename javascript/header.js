0a8749b3ab255f9069285aa792af7b57e39c3312
daniel mihai <sasdaniel9@gmail.com>
2021-08-06 09:11:23 +0100

    font modifications and CV changing

--- javascript/header.js
+++ javascript/header.js
@@ -1,5 +1,16 @@
 const menu      = document.querySelector('.menu');
 const links     = document.querySelector('.nav-links');
+const navigatoAbout = document.querySelector('.navigatoAbout');
+const navBottomContact = document.querySelector('.navBottomContact');
+
+
+navBottomContact.addEventListener('click', () => {
+    window.scrollTo(0,document.body.scrollHeight);
+})
+
+navigatoAbout.addEventListener('click', () => {
+    location.href = '../pages/About.html';
+})
 
 menu.addEventListener('click', () => {
     links.classList.toggle('display');
--- javascript/index.js
+++ javascript/index.js
@@ -6,11 +6,17 @@ const restaurant    = document.querySelector('.ohmyfood');
 const reservia      = document.querySelector('.reservia-btn');
 const fitnessPal    = document.querySelector('.fitnessPal');
 
+/* Footer elements */
 const footerEmail   = document.querySelector('.footerEmail');
 const footerContact = document.querySelector('.footerContact');
 const footerCall    = document.querySelector('.footerCall');
 const footerGit     = document.querySelector('.footerGitNav');
 const navtoGit      = () => location.href = 'https://github.com/ddmihai';
+
+/* Middle section */
+const grupomaniaHRB = document.querySelector('.grupomaniaHRB');
+const pekokko = document.querySelector('.pekokko');
+
 
 /* Navigation buttons section */
 gitNav.addEventListener('click',            navtoGit);
@@ -23,3 +29,7 @@ footerGit.addEventListener('click',         () => location.href = 'https://githu
 restaurant.addEventListener('click',        () => location.href = 'https://ddmihai.github.io/restaurant/');
 reservia.addEventListener('click',          () => location.href = 'https://ddmihai.github.io/reservia.io/');
 fitnessPal.addEventListener('click',        () => location.href = 'https://flamboyant-stonebraker-c1c438.netlify.app/#/');
+
+
+grupomaniaHRB.addEventListener('click',     () => location.href = 'https://github.com/ddmihai/grupomania-backend');
+pekokko.addEventListener('click',           () => location.href = 'https://github.com/ddmihai/Hot-sauce-reviews');
